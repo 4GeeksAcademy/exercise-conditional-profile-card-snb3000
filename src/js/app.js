7b07c4037fdc08d836a0739dda0988f455e174a0
Shafik Bahou <snb3000@gmail.com>
2024-06-13 00:50:53 +0000

    updated JS for social media

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ import "../style/index.css";
     {
         includeCover: true, // if includeCover is true the algorithm should show the cover image
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/man/33.jpg", // this is the url for the profile avatar
+        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
         socialMediaPosition: "right", // social media bar position (left or right)
         
         twitter: null, // social media usernames
@@ -81,7 +81,7 @@ window.onload = function() {
     // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://randomuser.me/api/portraits/women/43.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
